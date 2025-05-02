c5f5efa0e2b59b0dfdf104a07d03252452b2f4d8
Douglas Florindo <douglasflorindodeassis@gmail.com>
2025-05-02 12:37:04 -0300

    Instancia MainWindowViewModel e método OnNewChatConnection para instanciar novos chats

--- Views/MainWindow.axaml.cs
+++ Views/MainWindow.axaml.cs
@@ -1,21 +1,25 @@
 using Avalonia.Controls;
 using Avalonia.Interactivity;
 using System.Diagnostics;
+using TCPChatGUI.Connection;
 using TCPChatGUI.ViewModels;
 
 namespace TCPChatGUI.Views;
 
 public partial class MainWindow : Window
 {
+    private readonly MainWindowViewModel ViewModel;
     public MainWindow()
     {
         InitializeComponent();
-        var viewModel = new MainWindowViewModel();
-        viewModel.ClientConnected += (sender, connection) =>
-        {
-            var chatWindow = new Chat(connection);
-            chatWindow.Show();
-        };
-        DataContext = viewModel;
+        ViewModel = new();
+        ViewModel.NewChatConnection += OnNewChatConnection;
+        DataContext = ViewModel;
+    }
+
+    public void OnNewChatConnection(object? sender, NewChatConnectionEventArgs e)
+    {
+        var chatWindow = new Chat(e.Connection);
+        chatWindow.Show();
     }
 }
