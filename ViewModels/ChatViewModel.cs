d7eed2097410500790419047ed7e130089a1899b
Douglas Florindo <douglasflorindodeassis@gmail.com>
2025-05-02 15:39:52 -0300

    Métodos SendMessage, OnDataReceived, etc

--- ViewModels/ChatViewModel.cs
+++ ViewModels/ChatViewModel.cs
@@ -2,6 +2,8 @@
 using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.Net.Sockets;
+using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
@@ -18,17 +20,30 @@ public partial class ChatViewModel : ViewModelBase
     /// </summary>
     private readonly ChatConnection _chatConnection;
 
+    /// <summary>
+    /// Conteúdo da mensagem a ser enviada. Sempre que o conteúdo desta propriedade é alterado, o comando <see cref="SendMessage"/> é invalidado (e então será validado ou não pelo método <see cref="CanSendMessage"/>) 
+    /// </summary>
+    [ObservableProperty]
+    [NotifyCanExecuteChangedFor(nameof(SendMessageCommand))]
+    private string? _newMessageContent;
+
+    public ObservableCollection<MessageViewModel> Messages { get; } = [];
+
+    
+
+
     public ChatViewModel(ChatConnection chatConnection)
     {
         _chatConnection = chatConnection;
         _chatConnection.TextReceived += OnTextReceived;
+        _chatConnection.DataReceived += OnDataReceived;
 
-        // _ = _chatConnection.ReadText();
+        _ = _chatConnection.ReadData();
         _ = Task.Run(() => ReadConsoleInputAsync());
     }
 
     /// <summary>
-    /// Lida com o texto emitido pelo evento <see cref="TextReceived"/>.
+    /// Lida com o texto emitido pelo evento <see cref="TextReceived"/> (emitido pela classse <see cref="ChatConnection"/>).
     /// </summary>
     /// <param name="sender"></param>
     /// <param name="e"></param>
@@ -54,60 +69,67 @@ public partial class ChatViewModel : ViewModelBase
         }
     }
 
+    /// <summary>
+    /// Lida com os dados emitidos pelo evento <see cref="DataReceived"/> (emitido pela classse <see cref="ChatConnection"/>).
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private void OnDataReceived(object? sender, Connection.DataReceivedEventArgs e)
+    {
+        try
+        {
+            string jsonMessage = Encoding.UTF8.GetString(e.Data);
+            var message = JsonSerializer.Deserialize<Message>(jsonMessage);
 
-    public EventHandler<TextReceivedEventArgs>? TextReceived;
+            if (message != null)
+            {
+                Messages.Add(new MessageViewModel(message));
 
+                // Dispara o evento de nova mensagem recebida.
+                MessageReceived?.Invoke(this, new MessageReceivedEventArgs(message));
 
-
-    // =================== TO-DO ===========================
-
-
-
-    // private void OnDataReceived()
-    // {
-
-    // }
-
-
-    // private async Task SendMessage(Message message)
-    // {
-
-    // }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+                Console.WriteLine($"Mensagem recebida: {message.Username}: {message.Content} - {message.Time}");
+            }
+            else
+            {
+                Console.WriteLine("Mensagem recebida não é válida.");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Erro ao processar dados recebidos: {ex.Message}");
+        }
+    }
 
 
+    private async Task SendMessage(Message message)
+    {
+        string jsonMessage = JsonSerializer.Serialize(message);
 
+        byte[] dataToSend = Encoding.UTF8.GetBytes(jsonMessage);
 
+        await _chatConnection.WriteData(dataToSend);
+    }
 
-
-
-    // ================ ignora isso por enquanto =================
-
-
-
-
-    public ObservableCollection<MessageViewModel> Messages { get; } = [];
 
     /// <summary>
-    /// Add new Message to the list. Only available if CanAddMessage is true.
+    /// Send message and add it to the list. Only available if <see cref="CanSendMessage"/> is true.
     /// </summary>
-    [RelayCommand(CanExecute = nameof(CanAddMessage))]
-    public void AddMessage()
+    [RelayCommand(CanExecute = nameof(CanSendMessage))]
+    public void SendMessage()
     {
-        Messages.Add(new MessageViewModel() { Username = "[User]", Content = NewMessageContent, Time = DateTime.Now });
+        var message = new Message
+        {
+            Username = "[User]",
+            Content = NewMessageContent,
+            Time = DateTime.Now
+        };
+
+        _ = SendMessage(message); 
+
+        var viewModel = new MessageViewModel(message);
+        Messages.Add(viewModel);
+
         NewMessageContent = null;
     }
 
@@ -121,19 +143,22 @@ public partial class ChatViewModel : ViewModelBase
     }
 
 
-    [ObservableProperty]
-    [NotifyCanExecuteChangedFor(nameof(AddMessageCommand))] // This attribute will invalidate the command each time this property changes
-    private string? _NewMessageContent;
+    
 
 
 
     /// <summary>
-    /// Returns if a new Message can be added. Messages must have text
+    /// Returns if a new Message can be send. Messages must have text.
     /// </summary>
-    public bool CanAddMessage()
+    public bool CanSendMessage()
     {
         return !string.IsNullOrWhiteSpace(NewMessageContent);
     }
+
+
+    public EventHandler<TextReceivedEventArgs>? TextReceived;
+
+    public EventHandler<MessageReceivedEventArgs>? MessageReceived;
 
 
 
