290c622af276473a53663f7bd4a381a5ed8943df
Douglas Florindo <douglasflorindodeassis@gmail.com>
2025-05-02 15:33:29 -0300

    métodos para ler e escrever bytes na stream

--- Connection/Connection.cs
+++ Connection/Connection.cs
@@ -33,7 +33,7 @@ public partial class ChatConnection : ObservableObject
 
     private bool _disposed;
 
-    
+
     public ChatConnection(NetworkStream stream, IPEndPoint endPoint)
     {
         Debug.WriteLine("Connection established.");
@@ -44,61 +44,6 @@ public partial class ChatConnection : ObservableObject
         _textReader = new(Stream, Encoding.UTF8, leaveOpen: true);
         _textWriter = new(Stream, Encoding.UTF8, leaveOpen: true) { AutoFlush = true };
 
-        _ = Task.Run(() => ReadText());
-    }
-
-
-    /// <summary>
-    /// Inicia a leitura exclusiva de texto por meio do <see cref="StreamReader"/>. Emite o evento <see cref="OnTextReceived"/> quando lê um texto no stream. 
-    /// </summary>
-    /// <returns></returns>
-    public async Task ReadText()
-    {
-        var buffer = new char[1024];
-
-        try
-        {
-            while (!_cancellationTokenSource.Token.IsCancellationRequested)
-            {
-                // Bloqueia a thread até que leia algo no stream.
-                int bytesRead = await _textReader.ReadAsync(buffer, 0, buffer.Length);
-                // Encerra a conexão quando lê exatamente 0 bytes (sinal de conexão encerrada).
-                if (bytesRead == 0)
-                {
-                    Debug.WriteLine("Connection closed by remote host");
-                    break;
-                }
-
-                string text = new(buffer, 0, bytesRead);
-                // Emite o evento de texto recebido
-                OnTextReceived(text.TrimEnd('\0'));
-            }
-        }
-        catch (OperationCanceledException)
-        {
-            Debug.WriteLine("Read operation canceled");
-        }
-        catch (IOException ex)
-        {
-            Debug.WriteLine($"Network error: {ex.Message}");
-        }
-        catch (Exception ex)
-        {
-            Debug.WriteLine($"Unexpected error: {ex}");
-        }
-        finally
-        {
-            CloseConnection();
-        }
-    }
-
-
-    /// <summary>
-    /// Encerra a leitura do stream.
-    /// </summary>
-    public void StopReading()
-    {
-        _cancellationTokenSource.Cancel();
     }
 
 
@@ -132,6 +77,95 @@ public partial class ChatConnection : ObservableObject
         }
     }
 
+
+
+    /// <summary>
+    /// Inicia a leitura exclusiva de texto por meio do <see cref="StreamReader"/>. Emite o evento <see cref="OnTextReceived"/> quando lê um texto no stream. 
+    /// </summary>
+    /// <returns></returns>
+    public async Task ReadText()
+    {
+        var buffer = new char[1024];
+
+        try
+        {
+            while (!_cancellationTokenSource.Token.IsCancellationRequested)
+            {
+                // Bloqueia a thread até que leia algo no stream.
+                int bytesRead = await _textReader.ReadAsync(buffer, 0, buffer.Length);
+                // Encerra a conexão quando lê exatamente 0 bytes (sinal de conexão encerrada).
+                if (bytesRead == 0)
+                {
+                    Debug.WriteLine("Connection closed by remote host");
+                    break;
+                }
+
+                string text = new(buffer, 0, bytesRead);
+                // Emite o evento de texto recebido
+                TextReceived?.Invoke(this, new TextReceivedEventArgs(text.TrimEnd('\0')));
+            }
+        }
+        catch (OperationCanceledException)
+        {
+            Debug.WriteLine("Read operation canceled");
+        }
+        catch (IOException ex)
+        {
+            // Lançado quando o leitor não consegue mais ler (conexão fechada).
+            Debug.WriteLine($"Network error: {ex.Message}");
+            throw;
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine($"Unexpected error: {ex}");
+        }
+        finally
+        {
+            CloseConnection();
+        }
+    }
+
+
+    public async Task ReadData()
+    {
+        var buffer = new Byte[256];
+
+        while (!_cancellationTokenSource.Token.IsCancellationRequested)
+        {
+            if (Stream == null || !Stream.CanRead) throw new InvalidOperationException("Stream não está disponível para leitura.");
+
+            int bytesRead = await Stream.ReadAsync(buffer);
+
+            if (bytesRead == 0) break;
+
+            var receivedData = new byte[bytesRead];
+            Array.Copy(buffer, receivedData, bytesRead);
+
+            DataReceived?.Invoke(this, new DataReceivedEventArgs(receivedData));
+
+        }
+    }
+
+
+    public async Task WriteData(Byte[] data)
+    {
+        if (Stream == null || !Stream.CanWrite) throw new InvalidOperationException("Stream não está disponível para escrita.");
+
+        await Stream.WriteAsync(data);
+        await Stream.FlushAsync();
+    }
+
+
+    /// <summary>
+    /// Encerra a leitura do stream.
+    /// </summary>
+    public void StopReading()
+    {
+        _cancellationTokenSource.Cancel();
+    }
+
+
+
     /// <summary>
     /// Encerra a conexão
     /// </summary>
@@ -156,40 +190,12 @@ public partial class ChatConnection : ObservableObject
         }
     }
 
-   
-    protected void OnTextReceived(string text)
-    {
-        TextReceived?.Invoke(this, new TextReceivedEventArgs(text));
-    }
-
     public event EventHandler<TextReceivedEventArgs>? TextReceived;
 
 
+    public event EventHandler<DataReceivedEventArgs>? DataReceived;
 
-
-
-
-
-    // ======================= TO-DO ====================================
-
-
-
-
-    // public async Task ReadData()
-    // {
-    //     while (true)
-    //     {
-    //         var buffer = new Byte[256];
-    //         var data = await Stream.ReadAsync(buffer);
-
-    //     }
-    // }
-
-
-    // public void WriteData(Byte[] data)
-    // {
-    //     Stream.Write()
-    // }
 
 
 }
+
